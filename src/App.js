7dd5646a281e01c3676e1f029983bf2689ed1d36
[Lakshya Bhati] <[lakshyabhati0071@gmail.com]>
2024-07-07 21:20:14 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const alankey='';
+
+const App=()=>{
+  useEffect(()=>{
+    alanBtn({
+      key:alankey,
+      onCommand:({command})=>{
+        if(command === 'testCommand'){
+          alert("This Code Was executed");
+        }
+      }
+    })
+  },[])
+  return(
+    <div>
+      <h1>AI NEWS APPLICATION</h1>
     </div>
   );
 }
